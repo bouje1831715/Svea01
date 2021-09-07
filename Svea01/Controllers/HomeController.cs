0c3f358e239f77e730c94fd21cf445ba9ab95c84
1831715 <1831715@etu.cegepjonquiere.ca>
2021-09-07 07:04:27 -0700

    test

--- Svea01/Controllers/HomeController.cs
+++ Svea01/Controllers/HomeController.cs
@@ -6,20 +6,36 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Svea01.Models;
 
 namespace Svea01.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Random _random = new Random();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        public int[] yeet(int value)
+        {
+
+            int[] rndData = new int[value];
+
+            for (int i = 0; i < value; i++)
+            {
+                rndData[i] = _random.Next(0, 1000);
+            }
+
+            return rndData;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.arrayData = yeet(100);
             return View();
         }
 
